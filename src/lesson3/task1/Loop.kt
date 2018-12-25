90cae50ffadd2fc96d1ec98d50a71dd29b03951a
Anna <l_a_a@yahoo.com>
2018-12-25 14:55:25 +0300

    Lesson 3

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -1,5 +1,9 @@
 @file:Suppress("UNUSED_PARAMETER")
+
 package lesson3.task1
+
+import lesson1.task1.sqr
+import kotlin.math.abs
 import kotlin.math.pow
 import kotlin.math.sqrt
 
@@ -72,10 +76,10 @@ fun digitNumber(n: Int): Int {
     do {
         count++
         number /= 10
-    }
-while (number != 0)
+    } while (number != 0)
     return count
 }
+
 /**
  * Простая
  *
@@ -85,9 +89,8 @@ while (number != 0)
 fun fib(n: Int): Int {
     var fib1 = 1
     var sum = 1
-    if(n < 3) return 1
-    else for (i in 3..n)
-{
+    if (n < 3) return 1
+    else for (i in 3..n) {
         sum += fib1
         fib1 = sum - fib1
     }
@@ -100,21 +103,58 @@ fun fib(n: Int): Int {
  * Для заданных чисел m и n найти наименьшее общее кратное, то есть,
  * минимальное число k, которое делится и на m и на n без остатка
  */
-fun lcm(m: Int, n: Int): Int = TODO()
+// Решение через НОД
+fun gsd(m: Int, n: Int): Int {
+    var a = abs(n)
+    var b = abs(m)
+    while ((a > 0 && b > 0)) {
+        if (a > b)
+            a %= b
+        else b %= a
+    }
+    return a + b
+}
+
+fun lcm(m: Int, n: Int): Int = (abs(m) * abs(n)) / gsd(m, n)
 
 /**
  * Простая
  *
  * Для заданного числа n > 1 найти минимальный делитель, превышающий 1
  */
-fun minDivisor(n: Int): Int = TODO()
+fun minDivisor(n: Int): Int {
+    //Наименьший простой делитель составного числа а не превосходит √а
+    var result = 0
+    if (n % 2 == 0) return 2
+    else if (isPrime(n)) return n
+    else
+        for (i in 3..sqrt(n.toDouble()).toInt() step 2) {
+            if ((n % i) > 0) continue
+            result += i
+            break
+        }
+    return result
+}
 
 /**
  * Простая
  *
  * Для заданного числа n > 1 найти максимальный делитель, меньший n
  */
-fun maxDivisor(n: Int): Int = TODO()
+fun maxDivisor(n: Int): Int {
+    var result = 0
+    when {
+        isPrime(n) -> return 1
+        n % 2 == 0 -> return (n / 2)
+        else -> for (i in n / 2 downTo 3) {
+            if (n % i == 0) {
+                result += i
+                return result
+            }
+        }
+    }
+    return result
+}
 
 /**
  * Простая
@@ -123,7 +163,22 @@ fun maxDivisor(n: Int): Int = TODO()
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean = TODO()
+// Решение через НОД
+fun isCoPrime(m: Int, n: Int): Boolean = gsd(m, n) == 1
+
+/*//Другое
+fun isCoPrime(m: Int, n: Int): Boolean {
+    var result = 0
+    val number = (min(m, n))
+    for (i in 2..number) {
+        if (((n % i) == 0) && ((m % i) == 0))
+{
+            result += i
+            break
+}
+    }
+    return result == 0
+}*/
 
 /**
  * Простая
@@ -132,7 +187,15 @@ fun isCoPrime(m: Int, n: Int): Boolean = TODO()
  * то есть, существует ли такое целое k, что m <= k*k <= n.
  * Например, для интервала 21..28 21 <= 5*5 <= 28, а для интервала 51..61 квадрата не существует.
  */
-fun squareBetweenExists(m: Int, n: Int): Boolean = TODO()
+
+fun squareBetweenExists(m: Int, n: Int): Boolean {
+    var result = 0
+    for (i in sqrt(m.toDouble()).toInt()..sqrt(n.toDouble()).toInt() + 1)
+        if ((i * i <= n) && (i * i >= m)) {
+            result++
+        }
+    return result > 0
+}
 
 /**
  * Средняя
@@ -232,6 +295,27 @@ fun squareSequenceDigit(n: Int): Int {
     var number = 0
     var numberConst = 0
     var countResult = 0
+    for (i in 1..n) {
+        number += sqr(i)
+        numberConst = sqr(i)
+        while (number > 0) {
+            count += 1
+            number /= 10
+        }
+        while (count > 0) {
+            count -= 1
+            countResult += 1
+            if (countResult == n) return ((numberConst / (10.0.pow(count)).toInt()) % 10)
+        }
+    }
+    return ((numberConst / (10.0.pow(count)).toInt()) % 10)
+}
+
+/*{
+    var count = 0
+    var number = 0
+    var numberConst = 0
+    var countResult = 0
     for (i in 1..n)
 {
         number += i * i
@@ -247,7 +331,7 @@ fun squareSequenceDigit(n: Int): Int {
         }
     }
     return ((numberConst / (10.0.pow(count)).toInt()) % 10)
-}
+}*/
 
 /**
  * Сложная
@@ -265,8 +349,7 @@ fun fibSequenceDigit(n: Int): Int {
     var number = 0
     var numberConst = 0
     var countResult = 0
-    for (i in 1..n)
-{
+    for (i in 1..n) {
         // фиб 1 - это фиб 1, фиб 2 - это сумма
         sum += firstFib
         number += sum
